bdd0e854e26e4ed1e2f3516a39336c83dbbcc971
Mina Lui <minamleung@gmail.com>
2023-08-01 15:43:10 +0100

    style landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,25 +1,69 @@
-import { useState, useEffect } from "react";
-import { getPhotoOfTheDayData } from "../api/nasaApi";
-import Button from "../components/Button";
+import "../styles/_variables.scss";
+import iconBCard from "../assets/icon-bcard.png";
+import iconQuiz from "../assets/icon-quiz.png";
+import iconRovers from "../assets/icon-rovers.png";
+import iconWeather from "../assets/icon-weather.png";
+import "./landing-page.scss";
 
+interface PageButtons {
+  name: string;
+  icon: string;
+  heading: string;
+  background: string;
+}
 const Home = () => {
-  const [photoOfTheDayTitle, setPhotoOfTheDayTitle] = useState<string>();
-  const [photoOfTheDayUrl, setPhotoOfTheDayUrl] = useState<string>();
-
-  useEffect(() => {
-    getPhotoOfTheDayData().then((data) => {
-      setPhotoOfTheDayTitle(data.title);
-      setPhotoOfTheDayUrl(data.url);
-    });
-  }, []);
+  const buttonsArray: PageButtons[] = [
+    {
+      name: "rovers",
+      icon: iconRovers,
+      heading: "Discover Rovers",
+      background: "dashboard-navigation-item__rovers",
+    },
+    {
+      name: "bCard",
+      icon: iconBCard,
+      heading: "Make a Birthday Card",
+      background: "dashboard-navigation-item__bCard",
+    },
+    {
+      name: "quiz",
+      icon: iconQuiz,
+      heading: "Take our Quiz",
+      background: "dashboard-navigation-item__quiz",
+    },
+    {
+      name: "weather",
+      icon: iconWeather,
+      heading: "Check the weather on Mars",
+      background: "dashboard-navigation-item__weather",
+    },
+  ];
 
   return (
-    <div>
-      <h1>Mars Mission</h1>
-      {photoOfTheDayUrl && (
-        <img alt={photoOfTheDayTitle} src={photoOfTheDayUrl} />
-      )}
-      <Button onClick={() => alert("Hello!")}>Click me</Button>
+    <div className="landing-page-wrapper">
+      <div className="heading-wrapper">
+        <div className="mars-wrapper headings-wrapper">
+          <span className="heading-text">MARS</span>
+        </div>
+        <div className="mission-wrapper headings-wrapper">
+          <span className="heading-text">MISSION</span>
+        </div>
+      </div>
+
+      <div className="dashboard-navigation">
+        {buttonsArray.map((button) => (
+          <div className="dashboard-navigation-item">
+            <div
+              className={`dashboard-navigation-item-icon ${button.background}`}
+            >
+              <img src={button.icon} alt={button.name} />
+            </div>
+            <button className="dashboard-navigation-button" key={button.name}>
+              {button.heading}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
