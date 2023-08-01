55a810a07672341320297b3851c04249ad2bc04e
D K <none>
2023-08-01 15:28:24 +0100

    29 questions created

--- data/quizdata.ts
+++ data/quizdata.ts
@@ -54,6 +54,232 @@ const questions: Question[] = [
       { optionText: "Five", isCorrect: false },
     ],
   },
+  {
+    questionText: "How long is a Mars year in Mars sols/days?",
+    options: [
+      { optionText: "1,000,000", isCorrect: false },
+      { optionText: "499.6", isCorrect: false },
+      { optionText: "599.6", isCorrect: false },
+      { optionText: "699.6", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "One of the Mars NASA rovers is called?",
+    options: [
+      { optionText: "Marty", isCorrect: false },
+      { optionText: "InSight", isCorrect: false },
+      { optionText: "Ingenuity", isCorrect: false },
+      { optionText: "Curiosity", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "One of the Mars NASA rovers is called?",
+    options: [
+      { optionText: "Perseverance", isCorrect: true },
+      { optionText: "InSight", isCorrect: false },
+      { optionText: "Ingenuity", isCorrect: false },
+      { optionText: "Marty", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The Mars NASA helicopter is called?",
+    options: [
+      { optionText: "Perseverance", isCorrect: false },
+      { optionText: "InSight", isCorrect: false },
+      { optionText: "Ingenuity", isCorrect: true },
+      { optionText: "Marty", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the name of the Mars NASA lander?",
+    options: [
+      { optionText: "Perseverance", isCorrect: false },
+      { optionText: "InSight", isCorrect: true },
+      { optionText: "Ingenuity", isCorrect: false },
+      { optionText: "Marty", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the radius of planet Mars?",
+    options: [
+      { optionText: "3,390km", isCorrect: true },
+      { optionText: "3,990km", isCorrect: false },
+      { optionText: "9,930km", isCorrect: false },
+      { optionText: "3,090km", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How much oxygen is there in the Mars atmosphere?",
+    options: [
+      { optionText: "An abundant amount", isCorrect: false },
+      { optionText: "None", isCorrect: false },
+      { optionText: "Tiny amount / trace amount", isCorrect: true },
+      { optionText: "Unknown", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "What percentage of the Mars Atmosphere is carbon dioxide (CO2)?",
+    options: [
+      { optionText: "0.4%", isCorrect: false },
+      { optionText: "45%", isCorrect: false },
+      { optionText: "95%", isCorrect: true },
+      { optionText: "0.04%", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How much water vapour is there in the Mars atmosphere?",
+    options: [
+      { optionText: "An abundant amount", isCorrect: false },
+      { optionText: "None", isCorrect: false },
+      { optionText: "Tiny amount / trace amount", isCorrect: true },
+      { optionText: "Unknown", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which of the following is a Mars moon?",
+    options: [
+      { optionText: "Phoebe", isCorrect: false },
+      { optionText: "Phobia", isCorrect: false },
+      { optionText: "Phobos", isCorrect: true },
+      { optionText: "Phil", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which of the following is a Mars moon?",
+    options: [
+      { optionText: "Demos", isCorrect: false },
+      { optionText: "Dimos", isCorrect: false },
+      { optionText: "Demis", isCorrect: false },
+      { optionText: "Deimos", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "What shape are the moons of Mars?",
+    options: [
+      { optionText: "Turnip", isCorrect: false },
+      { optionText: "Swede", isCorrect: false },
+      { optionText: "Carrot", isCorrect: false },
+      { optionText: "Potato", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "The moons of Mars are named after what?",
+    options: [
+      { optionText: "Ponies", isCorrect: false },
+      { optionText: "Donkeys", isCorrect: false },
+      { optionText: "Horses", isCorrect: true },
+      { optionText: "Dogs", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Mars is named after the God of?",
+    options: [
+      { optionText: "Love", isCorrect: false },
+      { optionText: "Wisdom", isCorrect: false },
+      { optionText: "Fire", isCorrect: false },
+      { optionText: "War", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Why is Mars rusty coloured?",
+    options: [
+      { optionText: "It's covered in tomato ketchup", isCorrect: false },
+      { optionText: "There are lots of active volcanoes", isCorrect: false },
+      {
+        optionText: "Iron minerals in the Martian soil oxidize",
+        isCorrect: true,
+      },
+      { optionText: "Its atmospheric gases glow red", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Mars is named after which Greek god?",
+    options: [
+      { optionText: "Ares", isCorrect: true },
+      { optionText: "Zeus", isCorrect: false },
+      { optionText: "Apollo", isCorrect: false },
+      { optionText: "Hera", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What distance is Mars from the Sun?",
+    options: [
+      { optionText: "288 million km", isCorrect: false },
+      { optionText: "828 million km", isCorrect: false },
+      { optionText: "882 million km", isCorrect: false },
+      { optionText: "228 million km", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "How long does it take light to travel from the Sun to Mars?",
+    options: [
+      { optionText: "11 minutes", isCorrect: false },
+      { optionText: "13 minutes", isCorrect: true },
+      { optionText: "31 minutes", isCorrect: false },
+      { optionText: "1 minute", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "One astronomical unit (AU), is the distance from the Sun to Earth. How many AU units is Mars from the Sun?",
+    options: [
+      { optionText: "0.5", isCorrect: false },
+      { optionText: "1", isCorrect: false },
+      { optionText: "1.5", isCorrect: true },
+      { optionText: "2", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "What is Mars' axis of rotation / tilt with respect to the plane of its orbit around the Sun?",
+    options: [
+      { optionText: "5 degrees", isCorrect: false },
+      { optionText: "2 degrees", isCorrect: false },
+      { optionText: "25 degrees", isCorrect: true },
+      { optionText: "52 degrees", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Mars has an elliptical (egg-shaped) orbit around the Sun. Which season in the northern hemisphere is the longest season at 194 sols?",
+    options: [
+      { optionText: "Winter", isCorrect: false },
+      { optionText: "Spring", isCorrect: true },
+      { optionText: "Summer", isCorrect: false },
+      { optionText: "Autumn", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Mars has an elliptical (egg-shaped) orbit around the Sun. Which season in the southern hemisphere is the longest season at 194 sols?",
+    options: [
+      { optionText: "Winter", isCorrect: false },
+      { optionText: "Spring", isCorrect: false },
+      { optionText: "Summer", isCorrect: false },
+      { optionText: "Autumn", isCorrect: true },
+    ],
+  },
+  {
+    questionText:
+      "Mars has an elliptical (egg-shaped) orbit around the Sun. Which season in the southern hemisphere is the shortest season at 142 sols?",
+    options: [
+      { optionText: "Winter", isCorrect: false },
+      { optionText: "Spring", isCorrect: true },
+      { optionText: "Summer", isCorrect: false },
+      { optionText: "Autumn", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Mars has an elliptical (egg-shaped) orbit around the Sun. Which season in the northern hemisphere is the shortest season at 142 sols?",
+    options: [
+      { optionText: "Winter", isCorrect: false },
+      { optionText: "Spring", isCorrect: false },
+      { optionText: "Summer", isCorrect: false },
+      { optionText: "Autumn", isCorrect: true },
+    ],
+  },
 ];
 
 export const getRandomQuestions = (): Question[] => {
@@ -61,4 +287,11 @@ export const getRandomQuestions = (): Question[] => {
     .map((q) => ({ q, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ q }) => q);
+};
+export const getNRandomQuestions = (n: number): Question[] => {
+  return questions
+    .map((q) => ({ q, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ q }) => q)
+    .slice(0, n);
 };
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { getPhotoOfTheDayData } from "../api/nasaApi";
 import Button from "../components/Button";
-import { getRandomQuestions } from "../../data/quizdata";
 
 const Home = () => {
   const [photoOfTheDayTitle, setPhotoOfTheDayTitle] = useState<string>();
@@ -21,9 +20,6 @@ const Home = () => {
         <img alt={photoOfTheDayTitle} src={photoOfTheDayUrl} />
       )}
       <Button onClick={() => alert("Hello!")}>Click me</Button>
-      {getRandomQuestions().map((q) => (
-        <div>{q.questionText}</div>
-      ))}
     </div>
   );
 };
