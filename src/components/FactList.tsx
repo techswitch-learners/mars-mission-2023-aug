443fc90fad76cd1cf8f9d3d0dcb7485d3e1e618b
D K <none>
2023-08-02 15:57:35 +0100

    mm-36 initial styling

--- src/components/FactList.tsx
+++ src/components/FactList.tsx
@@ -1,34 +1,34 @@
 import { getFacts } from "../data/factData";
+import "../styles/factlist.scss";
 
 const FactList = () => {
   return (
-    <div className="fact-list">
-      <h1 className="fact-list__header">FactsPage</h1>
-      {getFacts().map((section) => (
-        <div className="fact-list-section">
-          <h2 className="fact-list-section__title">{section.sectionTitle}</h2>
-          <div className="fact-list-section-fact">
-            {section.facts.map((fact) => (
-              <ul>
-                <li className="fact-list-section-fact-content">
-                  <h3 className="fact-list-section-fact-content__title">
-                    {fact.factTitle}
-                  </h3>
-                  <p className="fact-list-section-fact-content__text">
-                    {fact.factText}
-                  </p>
-                  <img
-                    className="fact-list-section-fact-content__image"
-                    src={fact.factImage}
-                    alt="factImage"
-                  />
-                </li>
-              </ul>
-            ))}
+    <div className="facts-wrapper">
+      <h1 className="facts-header">Fun Facts</h1>
+      <div className="facts-list">
+        {getFacts().map((section) => (
+          <div className="">
+            <h2 className="section-header">{section.sectionTitle}</h2>
+            <div className="">
+              {section.facts.map((fact) => (
+                <ul>
+                  <li className="">
+                    <h3 className="fact-header">{fact.factTitle}</h3>
+                    <div className="card">
+                      <img
+                        className="fact-image"
+                        src={fact.factImage}
+                        alt="factImage"
+                      />
+                      <p className="fact-text"> {fact.factText} </p>
+                    </div>
+                  </li>
+                </ul>
+              ))}
+            </div>
           </div>
-          <hr></hr>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
