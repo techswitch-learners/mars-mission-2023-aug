4f97d9a7716eb30818a741d89767d60078128eb8
Arda Kurt <to.arda.kurt@gmail.com>
2023-08-02 12:38:45 +0100

    Landing Page Nesting and Layout Fix

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,71 +1,79 @@
-import "../styles/_variables.scss";
-import iconBCard from "../assets/icon-bcard.png";
+import { Link } from "react-router-dom";
+import iconBirthdayCard from "../assets/icon-birthdaycard.png";
 import iconQuiz from "../assets/icon-quiz.png";
 import iconRovers from "../assets/icon-rovers.png";
 import iconWeather from "../assets/icon-weather.png";
-import "./landing-page.scss";
+import "./LandingPage.scss";
 
-interface PageButtons {
+interface PageButton {
   name: string;
   icon: string;
   heading: string;
-  background: string;
+  className: string;
+  path: string;
 }
-const Home = () => {
-  const buttonsArray: PageButtons[] = [
-    {
-      name: "rovers",
-      icon: iconRovers,
-      heading: "Discover Rovers",
-      background: "dashboard-navigation-item__rovers",
-    },
-    {
-      name: "bCard",
-      icon: iconBCard,
-      heading: "Make a Birthday Card",
-      background: "dashboard-navigation-item__bCard",
-    },
-    {
-      name: "quiz",
-      icon: iconQuiz,
-      heading: "Take our Quiz",
-      background: "dashboard-navigation-item__quiz",
-    },
-    {
-      name: "weather",
-      icon: iconWeather,
-      heading: "Check the weather on Mars",
-      background: "dashboard-navigation-item__weather",
-    },
-  ];
 
+const pageButtons: PageButton[] = [
+  {
+    name: "Rovers",
+    icon: iconRovers,
+    heading: "Discover Rovers",
+    className: "LandingPage__Dashboard__Item--rovers",
+    path: "/rovers",
+  },
+  {
+    name: "Birthday Card",
+    icon: iconBirthdayCard,
+    heading: "Make a Birthday Card",
+    className: "LandingPage__Dashboard__Item--birthdayCard",
+    path: "/birthday-card",
+  },
+  {
+    name: "Quiz",
+    icon: iconQuiz,
+    heading: "Take our Quiz",
+    className: "LandingPage__Dashboard__Item--quiz",
+    path: "/quiz",
+  },
+  {
+    name: "Weather",
+    icon: iconWeather,
+    heading: "Check the weather on Mars",
+    className: "LandingPage__Dashboard__Item--weather",
+    path: "/weather",
+  },
+];
+
+const LandingPage = () => {
   return (
-    <div className="landing-page-wrapper">
-      <div className="heading-wrapper">
-        <div className="mars-wrapper headings-wrapper">
-          <span className="heading-text">MARS</span>
-        </div>
-        <div className="mission-wrapper headings-wrapper">
-          <span className="heading-text">MISSION</span>
-        </div>
-      </div>
+    <div className="LandingPage">
+      <h1 className="LandingPage__Heading">
+        <span className="LandingPage__HeadingCapsule LandingPage__HeadingCapsule--mars">
+          MARS
+        </span>
+        <span className="LandingPage__HeadingCapsule LandingPage__HeadingCapsule--mission">
+          MISSION
+        </span>
+      </h1>
 
-      <div className="dashboard-navigation">
-        {buttonsArray.map((button) => (
-          <div className="dashboard-navigation-item">
-            <div
-              className={`dashboard-navigation-item-icon ${button.background}`}
-            >
-              <img src={button.icon} alt={button.name} />
+      <nav className="LandingPage__Dashboard">
+        {pageButtons.map((pageButton) => (
+          <Link to={pageButton.path} key={pageButton.name}>
+            <div className="LandingPage__Dashboard__Item">
+              <div
+                className={`LandingPage__Dashboard__Item--icon ${pageButton.className}`}
+              >
+                <img src={pageButton.icon} alt={pageButton.name} />
+              </div>
+              <div className="LandingPage__Dashboard__Item--tile">
+                {pageButton.heading}
+              </div>
             </div>
-            <button className="dashboard-navigation-button" key={button.name}>
-              {button.heading}
-            </button>
-          </div>
+          </Link>
         ))}
-      </div>
+      </nav>
     </div>
   );
 };
 
-export default Home;
+export default LandingPage;
