fdbd03839b5e4c6bd580478c5a9e3bf648867d20
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-02 11:08:24 +0100

    Added and styled Modal

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { GalleryPhotoDetails } from "../api/nasaApi";
+import "./Gallery.scss";
+import Button from "./Button";
 
 interface GalleryProps {
   galleryPhotos: GalleryPhotoDetails[];
@@ -10,7 +12,7 @@ const Gallery = ({ galleryPhotos }: GalleryProps) => {
     useState<GalleryPhotoDetails>();
 
   return (
-    <div>
+    <div className="Gallery">
       {galleryPhotos ? (
         galleryPhotos.map((photoDetails) => (
           <img
@@ -24,14 +26,27 @@ const Gallery = ({ galleryPhotos }: GalleryProps) => {
         <p>Loading...</p>
       )}
       {selectedPhotoDetails && (
-        <div>
-          <img src={selectedPhotoDetails.img_src} alt="Enlarged Image" />
-          <p>
-            Taken by the {selectedPhotoDetails.camera.full_name}, on{" "}
-            {selectedPhotoDetails.rover.name} on Martian sol{" "}
-            {selectedPhotoDetails.sol} (Earth Date{" "}
-            {selectedPhotoDetails.earth_date})
-          </p>
+        <div className="Modal__Backdrop">
+          <div className="Modal">
+            <Button
+              className="Modal__Close"
+              onClick={() => setSelectedPhotoDetails(undefined)}
+            >
+              ×
+            </Button>
+            <img
+              className="Gallery__ModalImage"
+              src={selectedPhotoDetails.img_src}
+              alt="Enlarged Image"
+              width={800}
+            />
+            <p>
+              Taken by the {selectedPhotoDetails.camera.full_name}, on{" "}
+              {selectedPhotoDetails.rover.name} on Martian sol{" "}
+              {selectedPhotoDetails.sol} (Earth Date{" "}
+              {selectedPhotoDetails.earth_date})
+            </p>
+          </div>
         </div>
       )}
     </div>
