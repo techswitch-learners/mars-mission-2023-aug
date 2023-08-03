0b931e1995133d995fd51426db102e99d08cf8ff
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-03 14:00:46 +0100

    WIP Rovers Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import PageWithNavBar from "./components/PageWithNavBar";
+import RoversPage from "./pages/RoversPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route index element={<LandingPage />} />
         <Route element={<PageWithNavBar />}>
           <Route path="/placeholder" element={<h1>Placeholder</h1>} />
+          <Route path="/rovers" element={<RoversPage />} />
         </Route>
       </Routes>
     </Router>
