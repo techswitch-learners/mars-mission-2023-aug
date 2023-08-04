6faadd31ed34f6242063425116c3e7467a1c5366
unknown <paul7markets@hotmail.com>
2023-08-04 10:48:54 +0100

    404 page css

--- src/pages/Error404Page.tsx
+++ src/pages/Error404Page.tsx
@@ -5,15 +5,15 @@ function Error404Page() {
   return (
     <div className="Error404Page">
       <h1>404</h1>
+      <p>It looks like you've got a bit lost</p>
       <img
         className="Error404Page__image"
         src="/image-404.jpeg"
         alt="404 image sad rover lost"
       />
-      <div>
-        <p>It looks like you've got a bit lost!</p>
-        <Link to="/">Take me Home</Link>
-      </div>
+      <Link className="Error404Page__Link" to="/">
+        Take me Home
+      </Link>
     </div>
   );
 }
